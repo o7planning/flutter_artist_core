ff632b9634a6817b35b3c8e55ec540e4a5b18ec3
o7planning <10128477+o7planning@users.noreply.github.com>
2025-07-02 01:59:44 +0600

    .

--- lib/src/_error/_api_error.dart
+++ lib/src/_error/_api_error.dart
@@ -20,4 +20,109 @@ class ApiError extends AppError {
     required super.errorMessage,
     super.errorDetails,
   });
+
+  static ApiError fromMap({
+    required int? status,
+    required String? statusMessage,
+    required Map<String, dynamic> map,
+  }) {
+    String errorMessage =
+        map['errorMessage'] ??
+        map['error'] ??
+        map['message'] ??
+        statusMessage ??
+        "Error";
+    //
+    dynamic errorDetailsX = map['errors'] ?? map['errorDetails'];
+    //
+    List<String>? errorDetails;
+    if (errorDetailsX != null) {
+      if (errorDetailsX is List) {
+        errorDetails =
+            errorDetailsX.map((detail) => detail.toString()).toList();
+      } else {
+        errorDetails = [errorDetailsX.toString()];
+      }
+    }
+    return ApiError(
+      status: status.toString(),
+      errorMessage: errorMessage,
+      errorDetails: errorDetails,
+    );
+  }
+
+  static ApiError fromResponseErrorData({
+    required int? status,
+    required String? statusMessage,
+    required dynamic responseErrorData,
+  }) {
+    if (responseErrorData == null) {
+      return ApiError(
+        status: status.toString(),
+        apiErrorType: ApiErrorType.unknown,
+        errorData: null,
+        errorMessage: statusMessage ?? "Unknown Error",
+        errorDetails: null,
+      );
+    }
+    if (responseErrorData is String) {
+      var jsonObj;
+      try {
+        jsonObj = jsonDecode(responseErrorData);
+      } catch (e) {
+        return ApiError(
+          status: status.toString(),
+          apiErrorType: ApiErrorType.unknown,
+          errorData: null,
+          errorMessage: statusMessage ?? "Unknown Error",
+          errorDetails: null,
+        );
+      }
+      if (jsonObj == null) {
+        return ApiError(
+          status: status.toString(),
+          apiErrorType: ApiErrorType.unknown,
+          errorData: null,
+          errorMessage: statusMessage ?? "Unknown Error",
+          errorDetails: null,
+        );
+      } else if (jsonObj is Map) {
+        return ApiError.fromMap(
+          status: status,
+          statusMessage: statusMessage,
+          map: jsonObj as Map<String, dynamic>,
+        );
+      } else if (jsonObj is List) {
+        return ApiError(
+          status: status.toString(),
+          apiErrorType: ApiErrorType.unknown,
+          errorData: null,
+          errorMessage: statusMessage ?? "Unknown Error (Array)",
+          errorDetails: null,
+        );
+      } else {
+        return ApiError(
+          status: status.toString(),
+          apiErrorType: ApiErrorType.unknown,
+          errorData: null,
+          errorMessage: statusMessage ?? "Unknown Error",
+          errorDetails: null,
+        );
+      }
+    } else if (responseErrorData is Map) {
+      return ApiError.fromMap(
+        status: status,
+        statusMessage: statusMessage,
+        map: responseErrorData as Map<String, dynamic>,
+      );
+    } else {
+      return ApiError(
+        status: status.toString(),
+        apiErrorType: ApiErrorType.unknown,
+        errorData: null,
+        errorMessage: statusMessage ?? "Unknown Error",
+        errorDetails: null,
+      );
+    }
+  }
 }
