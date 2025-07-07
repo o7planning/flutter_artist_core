bcd04a0ab826bccab6bea56e418a5e232048e8ad
o7planning <10128477+o7planning@users.noreply.github.com>
2025-07-08 02:13:13 +0600

    .

--- lib/src/_utils/_json_utils.dart
+++ lib/src/_utils/_json_utils.dart
@@ -21,4 +21,66 @@ class JsonUtils {
       return null;
     }
   }
+
+  static bool deepJsonCompare({
+    required Object? jsonObj1,
+    required Object? jsonObj2,
+  }) {
+    if (jsonObj1 == null && jsonObj2 == null) {
+      return true;
+    } else if (jsonObj1 == null && jsonObj2 != null) {
+      return false;
+    } else if (jsonObj1 != null && jsonObj2 == null) {
+      return false;
+    }
+    if (jsonObj1 is Map) {
+      if (jsonObj2 is List) {
+        return false;
+      } else if (jsonObj2 is Map) {
+        Set<dynamic> keys =
+            {}
+              ..addAll(jsonObj1.keys)
+              ..addAll(jsonObj2.keys);
+        for (dynamic key in keys) {
+          dynamic v1 = jsonObj1[key];
+          dynamic v2 = jsonObj1[key];
+          bool eq = deepJsonCompare(jsonObj1: v1, jsonObj2: v2);
+          if (!eq) {
+            return false;
+          }
+        }
+        return true;
+      } else {
+        return false;
+      }
+    } else if (jsonObj1 is List) {
+      if (jsonObj2 is List) {
+        if (jsonObj1.length != jsonObj2.length) {
+          return false;
+        }
+        for (int i = 0; i < jsonObj1.length; i++) {
+          bool eq = deepJsonCompare(
+            jsonObj1: jsonObj1[i],
+            jsonObj2: jsonObj2[i],
+          );
+          if (!eq) {
+            return false;
+          }
+        }
+        return true;
+      } else if (jsonObj2 is Map) {
+        return false;
+      } else {
+        return false;
+      }
+    } else {
+      if (jsonObj2 is List) {
+        return false;
+      } else if (jsonObj2 is Map) {
+        return false;
+      } else {
+        return jsonObj1 == jsonObj2;
+      }
+    }
+  }
 }
