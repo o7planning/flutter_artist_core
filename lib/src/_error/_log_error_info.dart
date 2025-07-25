5a82c731ebaf3ab4fcd54c902ae2e663b2177f64
o7planning <10128477+o7planning@users.noreply.github.com>
2025-07-25 20:14:42 +0600

    .

--- lib/src/_error/_log_error_info.dart
+++ lib/src/_error/_log_error_info.dart
@@ -3,10 +3,12 @@ part of '../../flutter_artist_core.dart';
 class LogErrorInfo extends ErrorInfo {
   final int id;
   final String? shelfName;
+  final String? methodName;
 
   LogErrorInfo({
     required this.id,
     required this.shelfName,
+    required this.methodName,
     required super.errorMessage,
     required super.errorDetails,
     required super.stackTrace,
